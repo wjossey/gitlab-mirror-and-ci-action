3ce0d1291a2f61e74914620ed95f7dda4d5f8d34
Weston Jossey <weston.jossey@gmail.com>
2020-06-21 16:17:57 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
 git checkout "${GITHUB_REF:11}"
 
-branch=${GITHUB_REPOSITORY}/$(git symbolic-ref --short HEAD)
+branch=$(git symbolic-ref --short HEAD)
 
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
